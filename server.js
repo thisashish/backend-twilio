79b91087e06e79eae8355c8c96f53c8363383cc8
Ashish Vishwakarma <ashish.vishwakarma1267@gmail.com>
2024-12-27 13:47:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const timeout = require('connect-timeout'); // Import timeout package
+const cors = require("cors"); // Import cors package
 
 // Routes
 const csvRoutes = require("./routes/csvRoutes");
@@ -12,7 +13,26 @@ const recordingRoutes = require("./routes/recordingRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS Configuration
+const allowedOrigins = [
+  "http://localhost:3000", // Allow localhost:3000
+  "https://assets.flex.twilio.com" // Allow Twilio Flex URL
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow the origin
+    } else {
+      callback(new Error("Not allowed by CORS")); // Reject the origin
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
+};
+
 // Middleware
+app.use(cors(corsOptions)); // Enable CORS with the specified options
 app.use(bodyParser.json());
 app.use(timeout('10s')); // Set a timeout duration (e.g., 10 seconds)
 
@@ -24,9 +44,9 @@ app.use((req, res, next) => {
 
 // Database Connection
 mongoose
-  .connect('mongodb://127.0.0.1:27017/hello', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  .connect(process.env.MONGO_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Database connection error:", err));
